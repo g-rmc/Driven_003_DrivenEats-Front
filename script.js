bbc4b65ed07032d842e414e823fa0eea493e3fa3
g-rmc <g.rmc3000@gmail.com>
2022-06-10 21:45:49 -0300

    Liberando botao de fechar pedido

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Definicao dos botoes de itens
 
+let prato;
+let bebida;
+let sobremesa;
+
 function SelecionarPrato(elemento) {
     let SelAnterior = document.querySelector(".prato .selecionado");
 
@@ -8,6 +12,10 @@ function SelecionarPrato(elemento) {
     }
 
     elemento.classList.add("selecionado");
+
+    prato = 1;
+
+    LiberarPedido();
 }
 
 function SelecionarBebida(elemento) {
@@ -18,9 +26,14 @@ function SelecionarBebida(elemento) {
     }
 
     elemento.classList.add("selecionado");
+
+    bebida = 1;
+
+    LiberarPedido();
 }
 
 function SelecionarSobremesa(elemento) {
+    
     let SelAnterior = document.querySelector(".sobremesa .selecionado");
 
     if (SelAnterior !== null) {
@@ -28,4 +41,23 @@ function SelecionarSobremesa(elemento) {
     }
 
     elemento.classList.add("selecionado");
+
+    sobremesa = 1;
+
+    LiberarPedido();
+}
+
+// Funcao de liberar para pedir comida
+
+function LiberarPedido() {
+
+    let BotaoBloqueado = document.querySelector(".pedido-bloqueado");
+    let BotaoLiberado = document.querySelector(".pedido-liberado");
+
+    if (prato && bebida && sobremesa) {
+        BotaoBloqueado.classList.add("hide");
+        BotaoLiberado.classList.remove("hide");
+    }
+        
+    
 }

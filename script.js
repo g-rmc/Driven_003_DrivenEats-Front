75405f3099b94ed4ea5ab529936d9284dba29978
g-rmc <g.rmc3000@gmail.com>
2022-06-11 11:26:59 -0300

    Finalizados os itens bonus

--- script.js
+++ script.js
@@ -92,3 +92,43 @@ function FecharPedido() {
     window.open("https://wa.me/?text=" + link)
 
 }
+
+// Bonus para tela de confirmação
+
+function Avancar() {
+    const Elemento = document.querySelector(".validar");
+    Elemento.classList.remove("hide");
+
+    let total = PratoPreco + BebidaPreco + SobremesaPreco;
+    
+    total = total.toFixed(2);
+    PratoPreco = PratoPreco.toFixed(2);
+    BebidaPreco = BebidaPreco.toFixed(2);
+    SobremesaPreco = SobremesaPreco.toFixed(2);
+
+    let nome = document.querySelector(".confirmacao .prato :nth-child(1)");
+    let preco = document.querySelector(".confirmacao .prato :nth-child(2)");
+    nome.innerHTML = PratoNome;
+    preco.innerHTML = PratoPreco;
+
+    nome = document.querySelector(".confirmacao .bebida :nth-child(1)");
+    preco = document.querySelector(".confirmacao .bebida :nth-child(2)");
+    nome.innerHTML = BebidaNome;
+    preco.innerHTML = BebidaPreco;
+
+    nome = document.querySelector(".confirmacao .sobremesa :nth-child(1)");
+    preco = document.querySelector(".confirmacao .sobremesa :nth-child(2)");
+    nome.innerHTML = SobremesaNome;
+    preco.innerHTML = SobremesaPreco;
+
+    preco = document.querySelector(".confirmacao .total :nth-child(2)");
+    preco.innerHTML = total;
+}
+
+function Voltar() {
+    const Elemento = document.querySelector(".validar");
+    Elemento.classList.add("hide");
+
+    // Quero resetar a seleção de pratos quando clicar em cancelar
+    document.location.reload();
+}
